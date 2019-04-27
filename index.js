613772ebd88ac27b6cff18391569c34a15a66c65
kchapelier <kevin.chapelier@gmail.com>
2019-04-27 16:44:20 +0200

    refactor to use a WebGL2 context

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-var utils = require('./lib/utils'),
-    parser = require('cellular-automata-rule-parser'),
-    generateShaders2D = require('./lib/cellular-automata-glsl-2d'),
-    generateShaders3D = require('./lib/cellular-automata-glsl-3d'),
-    GpuBackend = require('./lib/cellular-automata-gpu-backend'),
-    moore = require('moore'),
-    vonNeumann = require('von-neumann'),
-    unconventionalNeighbours = require('unconventional-neighbours');
+"use strict";
+
+var utils = require('./lib/utils');
+var parser = require('cellular-automata-rule-parser');
+var generateShaders2D = require('./lib/cellular-automata-glsl-2d');
+var generateShaders3D = require('./lib/cellular-automata-glsl-3d');
+var GpuBackend = require('./lib/cellular-automata-gpu-backend');
+var moore = require('moore');
+var vonNeumann = require('von-neumann');
+var unconventionalNeighbours = require('unconventional-neighbours');
 
 var neighbourhoodFunctions = {
-    'moore': moore,
-    'von-neumann': vonNeumann,
-    'axis': unconventionalNeighbours.axis,
-    'corner': unconventionalNeighbours.corner,
-    'edge': unconventionalNeighbours.edge,
-    'face': unconventionalNeighbours.face
+  'moore': moore,
+  'von-neumann': vonNeumann,
+  'axis': unconventionalNeighbours.axis,
+  'corner': unconventionalNeighbours.corner,
+  'edge': unconventionalNeighbours.edge,
+  'face': unconventionalNeighbours.face
 };
 
 /**
@@ -22,22 +24,22 @@ var neighbourhoodFunctions = {
  * @param {Array} b Second neighbour
  * @returns {number}
  */
-var neighbourhoodSorter = function neighbourhoodSorter (a, b) {
-    a = a.join(',');
-    b = b.join(',');
-    return a > b ? 1 : a < b ? -1 : 0;
-};
+function neighbourhoodSorter (a, b) {
+  a = a.join(',');
+  b = b.join(',');
+  return a > b ? 1 : a < b ? -1 : 0;
+}
 
-var getNeighbourhood = function getNeighbourhood(neighbourhoodType, neighbourhoodRange, dimension) {
-    neighbourhoodType = !!neighbourhoodFunctions[neighbourhoodType] ? neighbourhoodType : 'moore';
-    neighbourhoodRange = neighbourhoodRange || 1;
-    dimension = dimension || 2;
+function getNeighbourhood (neighbourhoodType, neighbourhoodRange, dimension) {
+  neighbourhoodType = !!neighbourhoodFunctions[neighbourhoodType] ? neighbourhoodType : 'moore';
+  neighbourhoodRange = neighbourhoodRange || 1;
+  dimension = dimension || 2;
 
-    var neighbourhood = neighbourhoodFunctions[neighbourhoodType](neighbourhoodRange, dimension);
-    neighbourhood.sort(neighbourhoodSorter);
+  var neighbourhood = neighbourhoodFunctions[neighbourhoodType](neighbourhoodRange, dimension);
+  neighbourhood.sort(neighbourhoodSorter);
 
-    return neighbourhood;
-};
+  return neighbourhood;
+}
 
 /**
  * CellularAutomataGpu constructor
@@ -45,20 +47,20 @@ var getNeighbourhood = function getNeighbourhood(neighbourhoodType, neighbourhoo
  * @param {int} [defaultValue=0] Default value of the cells
  * @constructor
  */
-var CellularAutomataGpu = function CellularAutomataGpu (shape, defaultValue) {
-    this.shape = shape;
-    this.dimension = shape.length;
+function CellularAutomataGpu (shape, defaultValue) {
+  this.shape = shape;
+  this.dimension = shape.length;
 
-    if (this.dimension !== 2 && this.dimension !== 3) {
-        throw new Error('CellularAutomataGpu does not support dimensions other than 2 and 3.');
-    }
+  if (this.dimension !== 2 && this.dimension !== 3) {
+    throw new Error('CellularAutomataGpu does not support dimensions other than 2 and 3.');
+  }
 
-    defaultValue = defaultValue || 0;
+  defaultValue = defaultValue || 0;
 
-    this.array = utils.createArray(shape, defaultValue);
-    this.backend = new GpuBackend(this.shape);
-    this.rules = [];
-};
+  this.array = utils.createArray(shape, defaultValue);
+  this.backend = new GpuBackend(this.shape);
+  this.rules = [];
+}
 
 CellularAutomataGpu.prototype.shape = null;
 CellularAutomataGpu.prototype.dimension = null;
@@ -79,32 +81,30 @@ CellularAutomataGpu.prototype.outOfBoundClamping = false;
  * @returns {CellularAutomataGpu} CellularAutomataGpu instance for method chaining.
  */
 CellularAutomataGpu.prototype.fillWithDistribution = function (distribution, rng) {
-    var sum = 0,
-        array = this.array.data,
-        numberOfDistributions = distribution.length,
-        selection,
-        i,
-        k;
+  var sum = 0;
+  var array = this.array.data;
+  var numberOfDistributions = distribution.length;
+  var selection, i, k;
 
-    rng = rng || Math.random;
+  rng = rng || Math.random;
+
+  for (i = 0; i < numberOfDistributions; i++) {
+    sum += distribution[i][1];
+  }
+
+  for (k = 0; k < array.length; k++) {
+    selection = rng() * sum;
 
     for (i = 0; i < numberOfDistributions; i++) {
-        sum += distribution[i][1];
+      selection -= distribution[i][1];
+      if (selection <= 0 && distribution[i][0] !== null) {
+        array[k] = distribution[i][0];
+        break;
+      }
     }
+  }
 
-    for (k = 0; k < array.length; k++) {
-        selection = rng() * sum;
-
-        for (i = 0; i < numberOfDistributions; i++) {
-            selection -= distribution[i][1];
-            if (selection <= 0 && distribution[i][0] !== null) {
-                array[k] = distribution[i][0];
-                break;
-            }
-        }
-    }
-
-    return this;
+  return this;
 };
 
 /**
@@ -114,29 +114,29 @@ CellularAutomataGpu.prototype.fillWithDistribution = function (distribution, rng
  * @returns {CellularAutomataGpu} CellularAutomataGpu instance for method chaining.
  */
 CellularAutomataGpu.prototype.setOutOfBoundValue = function (outOfBoundValue) {
-    if (outOfBoundValue === 'clamp') {
-        this.outOfBoundClamping = true;
-        this.outOfBoundWrapping = false;
-        this.outOfBoundValue = 0;
-    } else if (outOfBoundValue === 'wrap') {
-        this.outOfBoundClamping = false;
-        this.outOfBoundWrapping = true;
-        this.outOfBoundValue = 0;
-    } else {
-        this.outOfBoundClamping = false;
-        this.outOfBoundWrapping = false;
-        this.outOfBoundValue = outOfBoundValue | 0;
-    }
+  if (outOfBoundValue === 'clamp') {
+    this.outOfBoundClamping = true;
+    this.outOfBoundWrapping = false;
+    this.outOfBoundValue = 0;
+  } else if (outOfBoundValue === 'wrap') {
+    this.outOfBoundClamping = false;
+    this.outOfBoundWrapping = true;
+    this.outOfBoundValue = 0;
+  } else {
+    this.outOfBoundClamping = false;
+    this.outOfBoundWrapping = false;
+    this.outOfBoundValue = outOfBoundValue | 0;
+  }
 
-    if (this.currentRule !== null) {
-        this.currentRule = {
-            rule: this.currentRule.rule,
-            shaders: null,
-            iteration: 0
-        };
-    }
+  if (this.currentRule !== null) {
+    this.currentRule = {
+      rule: this.currentRule.rule,
+      shaders: null,
+      iteration: 0
+    };
+  }
 
-    return this;
+  return this;
 };
 
 /**
@@ -146,23 +146,23 @@ CellularAutomataGpu.prototype.setOutOfBoundValue = function (outOfBoundValue) {
  * @returns {CellularAutomataGpu} CellularAutomataGpu instance for method chaining.
  */
 CellularAutomataGpu.prototype.setRule = function (rule) {
-    var parsedRule = parser(rule);
+  var parsedRule = parser(rule);
 
-    if (rule === 'debug') {
-        parsedRule = { ruleFormat: 'debug' };
-    }
+  if (rule === 'debug') {
+    parsedRule = {ruleFormat: 'debug'};
+  }
 
-    if (parsedRule === null) {
-        throw new Error('The rulestring could not be parsed.');
-    }
+  if (parsedRule === null) {
+    throw new Error('The rulestring could not be parsed.');
+  }
 
-    this.currentRule = {
-        rule: parsedRule,
-        shaders: null,
-        iteration: 0
-    };
+  this.currentRule = {
+    rule: parsedRule,
+    shaders: null,
+    iteration: 0
+  };
 
-    return this;
+  return this;
 };
 
 /**
@@ -172,23 +172,23 @@ CellularAutomataGpu.prototype.setRule = function (rule) {
  * @returns {CellularAutomataGpu} CellularAutomataGpu instance for method chaining.
  */
 CellularAutomataGpu.prototype.iterate = function (iterationNumber) {
-    iterationNumber = iterationNumber || 1;
+  iterationNumber = iterationNumber || 1;
 
-    if (this.currentRule.iteration === 0) {
-        var neighbourhood = getNeighbourhood(this.currentRule.rule.neighbourhoodType, this.currentRule.rule.neighbourhoodRange, this.dimension),
-            outOfBoundValue = this.outOfBoundClamping ? 'clamp' : (this.outOfBoundWrapping ? 'wrap' : this.outOfBoundValue);
+  if (this.currentRule.iteration === 0) {
+    var neighbourhood = getNeighbourhood(this.currentRule.rule.neighbourhoodType, this.currentRule.rule.neighbourhoodRange, this.dimension);
+    var outOfBoundValue = this.outOfBoundClamping ? 'clamp' : (this.outOfBoundWrapping ? 'wrap' : this.outOfBoundValue);
 
-        if (this.dimension === 2) {
-            this.currentRule.shaders = generateShaders2D(this.currentRule.rule, neighbourhood, this.shape, this.backend.viewportWidth, this.backend.viewportHeight, outOfBoundValue);
-        } else if (this.dimension === 3) {
-            this.currentRule.shaders = generateShaders3D(this.currentRule.rule, neighbourhood, this.shape, this.backend.viewportWidth, this.backend.viewportHeight, outOfBoundValue);
-        }
-        this.rules.push(this.currentRule);
+    if (this.dimension === 2) {
+      this.currentRule.shaders = generateShaders2D(this.currentRule.rule, neighbourhood, this.shape, this.backend.viewportWidth, this.backend.viewportHeight, outOfBoundValue);
+    } else if (this.dimension === 3) {
+      this.currentRule.shaders = generateShaders3D(this.currentRule.rule, neighbourhood, this.shape, this.backend.viewportWidth, this.backend.viewportHeight, outOfBoundValue);
     }
+    this.rules.push(this.currentRule);
+  }
 
-    this.currentRule.iteration += iterationNumber;
+  this.currentRule.iteration += iterationNumber;
 
-    return this;
+  return this;
 };
 
 /**
@@ -199,7 +199,7 @@ CellularAutomataGpu.prototype.iterate = function (iterationNumber) {
  * @returns {CellularAutomataGpu} CellularAutomataGpu instance for method chaining.
  */
 CellularAutomataGpu.prototype.apply = function (rule, iteration) {
-    return this.setRule(rule).iterate(iteration);
+  return this.setRule(rule).iterate(iteration);
 };
 
 /**
@@ -208,25 +208,24 @@ CellularAutomataGpu.prototype.apply = function (rule, iteration) {
  * @returns {CellularAutomataGpu} CellularAutomataGpu instance for method chaining.
  */
 CellularAutomataGpu.prototype.finalize = function () {
-    if (this.rules.length) {
-        this.backend.write(this.array);
+  if (this.rules.length) {
+    this.backend.write(this.array);
 
-        for (var i = 0; i < this.rules.length; i++) {
-            this.backend.execute(this.rules[i]);
-        }
-
-        this.backend.read(this.array);
-
-        this.rules = [];
+    for (var i = 0; i < this.rules.length; i++) {
+      this.backend.execute(this.rules[i]);
     }
 
-    return this;
+    this.backend.read(this.array);
+
+    this.rules = [];
+  }
+
+  return this;
 };
 
 CellularAutomataGpu.prototype.destroy = function () {
-    this.backend.destroy();
-    this.backend = null;
+  this.backend.destroy();
+  this.backend = null;
 };
 
 module.exports = CellularAutomataGpu;
-
